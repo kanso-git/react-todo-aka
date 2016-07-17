99c05b075163bf97ae4827328dd87fe5825a1709
Abdallah Kanso <abdallah.kanso@gmail.com>
2016-07-18 00:44:15 +0200

    hide address bar

--- app/app.jsx
+++ app/app.jsx
@@ -17,6 +17,7 @@ require('style!css!sass!applicationStyles');
 
 
 ReactDOM.render(
+  
   <Router  history={hashHistory}>
     <Route path="/" component={Main}>
       <Route path="detail" component={Detail}/>
