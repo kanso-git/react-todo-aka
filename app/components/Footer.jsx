314af8d0ffd17011499c85daa627d7a95c904a8e
Abdallah Kanso <abdallah.kanso@gmail.com>
2016-07-20 02:28:22 +0200

    adjust styles

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,13 +2,14 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Footer = ()=>{
+  var styleLogo ="logo-bottom";
+      styleLogo+=  (/Mobi/.test(navigator.userAgent)?'' :'-desktop');
   return (
         <footer className="footer">
             <div className="row full-width" >
               <a herf="https://www.linkedin.com/in/abdallah-kanso-4789a68?trk=hp-identity-photo">
-                <img className="logo-bottom"
+                <img className={styleLogo}
                   src="https://cdn.prezly.com/8b/d789107d6711e5affe6d612b5698d9/140306_NES_AssociatedLogos_ATELIER_White.png"></img>
-
               </a>
               </div>
         </footer>
